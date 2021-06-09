53d15b49d8edb3cce1bec648139377ea96beb79f
Adithi Narayan <adithin2001@gmail.com>
2021-06-09 17:18:14 +0530

    Search party is here!

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -13,7 +13,9 @@ export default function Dashboard(){
     const { folderId } = useParams()
     const { state = {} } = useLocation()
     const { folder, childFolders, childFiles } = useFolder(folderId, state.folder)
+    const [folders, setFolders] = useState([])
     const [flag, wait]=useState(false)
+    useEffect(()=>setFolders([...childFolders]), [childFolders])
     useEffect(()=>{
         wait(false)
     },[folder])
@@ -26,11 +28,19 @@ export default function Dashboard(){
             <FolderPath currentFolder={folder}/>
             <AddFolder currentFolder={folder} />
             <AddFile currentFolder={folder}/>
+            <div className="search-bar">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#05505B"
+                     className="bi bi-search form-img" viewBox="0 0 18 18">
+                    <path
+                        d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                </svg>
+                <input className='form-input' placeholder='Search' type='text'  onChange={(e)=> setFolders([...childFolders.filter(x => x.name.startsWith(e.target.value))])}/>
+            </div>
         </div>
         <div className="folder-container">
             {childFolders.length > 0 && (
                 <div className="d-flex flex-wrap">
-                    {childFolders.map(childFolder => (
+                    {folders.map(childFolder => (
                         <div
                             key={childFolder.id}
                             style={{ maxWidth: "250px" }}
